3f6296c7c591037dc34ca52cdcd709d021945b8f
Ezequiel <ender.game.dev@gmail.com>
2024-08-14 19:58:48 -0300

    Updating HTML
    And started to create the JS

--- resources/script/app.js
+++ resources/script/app.js
@@ -1,5 +1,13 @@
-function sayHello() {
-    return "Hello World"
-}
+const encryptRadio =  document.getElementById("encrypt-radio");
+const decipherRadio = document.getElementById("decipher-radio");
+const submitButton = document.getElementById("submit-button");
 
-module.exports = sayHello
+submitButton.addEventListener("click", () => {
+    if (encryptRadio.checked) {
+        console.log(`You selected ${encryptRadio.value}`)
+    } else if (decipherRadio.checked) {
+        console.log(`You selected ${decipherRadio.value}`)
+    } else {
+        console.log("You must select an option");
+    }
+})
